122c2233ad179f8713f803c3c77f2f1b6ba974e3
goluhunk <singh1411gaurav@gmail.com>
2024-08-14 23:10:27 +0100

    priority updated

--- src/test/java/org/gaurav/TestCases.java
+++ src/test/java/org/gaurav/TestCases.java
@@ -9,12 +9,12 @@ public class TestCases {
         System.out.println("This is setup method");
     }
 
-    @Test(priority = 1)
+    @Test(priority = 2)
     public void login(){
         System.out.println("This is login method");
     }
 
-    @Test(priority = 2)
+    @Test(priority = 1)
     public void close(){
         System.out.println("This is close method");
     }
